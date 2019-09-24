6cf0d882a9a7e0d2c3086f8fb5dbd29f89f0180d
Pablo Cordero <pablocorderoromero@gmail.com>
2019-09-24 17:17:46 +0200

    P1 - Creado el modo chess Teminada a falta de correcion

--- P1/object3d.cc
+++ P1/object3d.cc
@@ -57,5 +57,22 @@ void _object3D::draw_fill()
 
 void _object3D::draw_chess()
 {
+  glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
+
+  glBegin(GL_TRIANGLES);
+  // glClearColor(255,255,255,0);
+  glColor3f(1,0,0);
+  for (unsigned int i=0;i<Triangles.size();i+=2){
+    glVertex3fv((GLfloat *) &Vertices[Triangles[i]._0]);
+    glVertex3fv((GLfloat *) &Vertices[Triangles[i]._1]);
+    glVertex3fv((GLfloat *) &Vertices[Triangles[i]._2]);
+  }
   
+  glColor3f(0,1,0);
+  for (unsigned int i=1;i<Triangles.size();i+=2){
+    glVertex3fv((GLfloat *) &Vertices[Triangles[i]._0]);
+    glVertex3fv((GLfloat *) &Vertices[Triangles[i]._1]);
+    glVertex3fv((GLfloat *) &Vertices[Triangles[i]._2]);
+  }
+  glEnd();
 }

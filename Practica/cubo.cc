d20f69425b51d1b24c40d4cd5e5d18c320957b72
Pablo Cordero <pablocorderoromero@gmail.com>
2019-12-09 16:59:43 +0100

    P5 - Textura correcta en el cubo. Falta luz

--- Practica/cubo.cc
+++ Practica/cubo.cc
@@ -19,38 +19,55 @@ Cubo::Cubo(float lado)
    v.push_back(v1);
    v.push_back(v2);
    v.push_back(v3);
-//    v.push_back(v4);
-//    v.push_back(v5);
-//    v.push_back(v6);
-//    v.push_back(v7);
+   v.push_back(v4);
+   v.push_back(v5);
+   v.push_back(v6);
+   v.push_back(v7);
+   v.push_back(v3); // 8
+   v.push_back(v7); // 9
+   v.push_back(v5); // 10
+   v.push_back(v6); // 11
 
    //Triangulos
-   Tupla3i  f0(0,1,3), f1(1,2,3);   // Cara de frente
-            // f2(1,5,2), f3(5,6,2),   // Cara de abajo
-            // f4(4,0,7), f5(0,3,7),   // Cara de arriba
-            // f6(7,6,4), f7(6,5,4),   // Cara posterior
-            // f8(3,2,7), f9(2,6,7),   // Cara derecha
-            // f10(4,5,0),f11(5,1,0);  // Cara izquierda
+   Tupla3i  f0(0,1,3), f1(1,2,3),   // Cara de frente
+            f2(1,5,2), f3(5,6,2),   // Cara de abajo
+            f4(4,0,7), f5(0,3,7),   // Cara de arriba
+            f6(7,6,4), f7(6,5,4),   // Cara posterior
+            f8(3,2,7), f9(2,6,7),   // Cara derecha
+            f10(4,5,0),f11(5,1,0),  // Cara izquierda
+            f12(4,0,9),f13(0,8,9),  // Cara superior duplicada
+            f14(1,10,2),f15(10,11,2);  // Cara inferior duplicada
 
    f.push_back(f0);
    f.push_back(f1);
-//    f.push_back(f2);
-//    f.push_back(f3);
-//    f.push_back(f4);
-//    f.push_back(f5);
-//    f.push_back(f6);
-//    f.push_back(f7);
-//    f.push_back(f8);
-//    f.push_back(f9);
-//    f.push_back(f10);
-//    f.push_back(f11);
+   f.push_back(f14); // Cara inferior duplicada para aplicar texturas
+   f.push_back(f15); // Cara inferior duplicada para aplicar texturas
+   f.push_back(f2);
+   f.push_back(f3);
+   f.push_back(f12); // Cara superior duplicada para aplicar texturas
+   f.push_back(f13); // Cara superior duplicada para aplicar texturas
+   f.push_back(f4);
+   f.push_back(f5);
+   f.push_back(f6);
+   f.push_back(f7);
+   f.push_back(f8);
+   f.push_back(f9);
+   f.push_back(f10);
+   f.push_back(f11);
 }
 
 void Cubo::calcular_texturas(){
-    ct.resize(4);
-ct[0] = {0,0};
-ct[1] = {0,1};
-ct[2] = {1,1};
-ct[3] = {1,0};
-
+    ct.resize(12);
+    ct[0] = {0,0};
+    ct[1] = {0,1};
+    ct[2] = {1,1};
+    ct[3] = {1,0};
+    ct[4] = {1,0};
+    ct[5] = {1,1};
+    ct[6] = {0,1};
+    ct[7] = {0,0};
+    ct[8] = {0,1};
+    ct[9] = {1,1};
+    ct[10] = {0,0};
+    ct[11] = {1,0};
 }
